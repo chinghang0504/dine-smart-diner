d1cab8c827188d205ab901797d15c5ffc2625bc9
Ching Hang Lam <chinghang0504@gmail.com>
2024-07-07 23:39:15 -0700

    Update the Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom'
 import './App.scss'
 import Menu from './pages/Menu/Menu';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<Navigate to="/menu" />} />
           <Route path="/menu" element={<Menu />} />
--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,23 +1,23 @@
 import './Menu.scss'
-import { getFoodTypes } from '../../services/database';
-import { useEffect, useState } from 'react';
-import FoodTypes from '../../components/FoodTypes/FoodTypes';
+// import { getFoodTypes } from '../../services/database';
+// import { useEffect, useState } from 'react';
+// import FoodTypes from '../../components/FoodTypes/FoodTypes';
 
 function menu() {
-  const [foodTypes, setFoodTypes] = useState([]);
+  // const [foodTypes, setFoodTypes] = useState([]);
 
-  useEffect(() => {
-    async function loadData() {
-      const foodTypes = await getFoodTypes();
-      setFoodTypes(foodTypes);
-    }
-    loadData();
-  });
+  // useEffect(() => {
+  //   async function loadData() {
+  //     const foodTypes = await getFoodTypes();
+  //     setFoodTypes(foodTypes);
+  //   }
+  //   loadData();
+  // });
 
   return (
     <>
       <div>menu</div>
-      <FoodTypes foodTypes={foodTypes} />
+      {/* <FoodTypes foodTypes={foodTypes} /> */}
     </>
   )
 }
